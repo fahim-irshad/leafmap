aa4303191523b1a39ef00a7118a27ee841476d1a
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2020-06-10 23:35:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-//import MyMap from "./MyMap";
+
+import React from "react";
 import { Map, TileLayer, Marker } from "react-leaflet";
-//import './App.css'
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
