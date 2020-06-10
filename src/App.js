00c8d4f0da441523eddcdf56105c272eed0d9fc3
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2020-06-10 23:22:08 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+//import MyMap from "./MyMap";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import './App.css'
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 50.0893,
+      lng: 14.4284,
+      zoom: 12
+    };
+  }
+ 
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[this.state.lat, this.state.lng]} />
+ 
+      </Map>
+    );
+  }
+ }
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
-}
-
-export default App;
+//export default App;
