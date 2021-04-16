ab55676b45d9034c6ca411922b8a68d0eebdfa5e
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2021-04-16 15:55:44 +0500

    Update MapView.js

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -25,7 +25,7 @@ class MapView extends Component {
     return (
       <Map center={currentLocation} zoom={zoom}>
         <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="OSM Base Map">
+        <LayersControl.BaseLayer checked name="Imagery Base Map">
         <TileLayer
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
           attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
