86cc1e6317181afbc43e7fcbd6d054b1413cef6d
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2021-04-26 11:56:02 +0500

    Update MarkerPopup.js

--- src/components/MarkerPopup.js
+++ src/components/MarkerPopup.js
@@ -7,10 +7,10 @@ const MarkerPopup = (props) => {
 
   return  (<Popup>
     <div className='poup-text'>
-  <span><b>Name:&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{CHART_NO}</span><br/>
-  <span><b>Title:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{TITLE}</span><br/>
-  <span><b>Scale:&nbsp;&nbsp;&nbsp;</b></span><span>{SCALE}</span><br/>
-  <span><b>Edition:&nbsp;&nbsp;</b></span><span>{EDITION}</span><br/>
+  <span><b>Name&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{CHART_NO}</span><br/>
+  <span><b>Title&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{TITLE}</span><br/>
+  <span><b>Scale&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{SCALE}</span><br/>
+  <span><b>Edition&nbsp;:&nbsp;&nbsp;</b></span><span>{EDITION}</span><br/>
   </div>
   </Popup>);
 };
