3f4728c64fd2741d82a1bb828a0791928faf7f86
fahim-irshad <66278214+fahim-irshad@users.noreply.github.com>
2021-04-28 12:07:04 +0500

    Update MarkerPopup.js

--- src/components/MarkerPopup.js
+++ src/components/MarkerPopup.js
@@ -6,11 +6,25 @@ const MarkerPopup = (props) => {
  
 
   return  (<Popup>
-    <div className='poup-text'>
-  <span><b>Name&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{CHART_NO}</span><br/>
-  <span><b>Title&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{TITLE}</span><br/>
-  <span><b>Scale&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span>{SCALE}</span><br/>
-  <span><b>Edition&nbsp;:&nbsp;&nbsp;</b></span><span>{EDITION}</span><br/>
+  <div className='poup-text'>
+  <table id='customers'> 
+  <tr>
+  <th><span><b>Name:</b></span></th>
+  <th><span>{CHART_NO}</span></th>
+  </tr>
+  <tr>
+  <th><span><b>Title:</b></span></th>
+  <th><span>{TITLE}</span></th>
+  </tr>
+  <tr>
+  <th><span><b>Scale:</b></span></th>
+  <th><span>{SCALE}</span></th>
+  </tr>
+  <tr>
+  <th><span><b>Edition:</b></span></th>
+  <th><span>{EDITION}</span></th>
+  </tr>
+   </table>
   </div>
   </Popup>);
 };
